032501f777e2c18e32101ee66f2c7c77b9efc03c
akhtarhssn <watchonlytutorial@gmail.com>
2026-06-03 13:50:15 +0600

    database-setup

---
+++ src/lib/db.ts
@@ -0,0 +1,25 @@
+import { Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@/generated/prisma/client";
+
+// Ensure the connection string exists before passing it
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL environment variable is missing.");
+}
+
+
+// Pass the configuration object directly into the PrismaNeon adapter
+const neonAdapter = new PrismaNeon({connectionString});
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const prisma =
+  globalForPrisma.prisma || new PrismaClient({ adapter: neonAdapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
