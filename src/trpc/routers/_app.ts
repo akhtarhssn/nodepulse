ad4f3935d0fbe75118ad121f200d57b66c289f57
akhtarhssn <watchonlytutorial@gmail.com>
2026-06-06 13:56:31 +0600

    added background jobs for workflow

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/db";
 import { createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inngest/client";
 export const appRouter = createTRPCRouter({
   getUsers: protectedProcedure.query(({ ctx }) => {
     return prisma.user.findMany({
@@ -7,6 +8,19 @@ export const appRouter = createTRPCRouter({
         id: ctx.auth.user.id,
       },
     });
+  }),
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async ({ ctx }) => {
+    await inngest.send({
+      name: "app/task.created",
+      data: {
+        id: "123",
+        email: "user@example.com",
+      },
+    });
+    return { success: true, message: "Workflow creation initiated" };
   }),
 });
 // export type definition of API
